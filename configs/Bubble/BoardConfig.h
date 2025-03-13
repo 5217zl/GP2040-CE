1a234e6dd767e7bd3c87676eaba271f7e0296dff
longzhan <80184216+5217zl@users.noreply.github.com>
2025-03-13 12:33:03 +0800

    Update BoardConfig.h

--- configs/Bubble/BoardConfig.h
+++ configs/Bubble/BoardConfig.h
@@ -14,7 +14,7 @@
 
 // Main pin mapping Configuration
 //                                                  // GP2040 | Xinput | Switch  | PS3/4/5  | Dinput | Arcade |
-#define GPIO_PIN_02 GpioAction::BUTTON_PRESS_S2     // UP     | UP     | UP      | UP       | UP     | UP     |
+#define GPIO_PIN_02 GpioAction::BUTTON_PRESS_UP     // UP_1   | UP_1   | UP_1    | UP_1     | UP_1   | UP_1   |
 #define GPIO_PIN_03 GpioAction::BUTTON_PRESS_DOWN   // DOWN   | DOWN   | DOWN    | DOWN     | DOWN   | DOWN   |
 #define GPIO_PIN_04 GpioAction::BUTTON_PRESS_RIGHT  // RIGHT  | RIGHT  | RIGHT   | RIGHT    | RIGHT  | RIGHT  |
 #define GPIO_PIN_05 GpioAction::BUTTON_PRESS_LEFT   // LEFT   | LEFT   | LEFT    | LEFT     | LEFT   | LEFT   |
@@ -27,11 +27,14 @@
 #define GPIO_PIN_12 GpioAction::BUTTON_PRESS_R1     // R1     | RB     | R       | R1       | 6      | P3     |
 #define GPIO_PIN_13 GpioAction::BUTTON_PRESS_L1     // L1     | LB     | L       | L1       | 5      | P4     |
 #define GPIO_PIN_16 GpioAction::BUTTON_PRESS_S1     // S1     | Back   | Minus   | Select   | 9      | Coin   |
-#define GPIO_PIN_17 GpioAction::BUTTON_PRESS_UP     // S2     | Start  | Plus    | Start    | 10     | Start  |
+#define GPIO_PIN_17 GpioAction::BUTTON_PRESS_S2     // S2     | Start  | Plus    | Start    | 10     | Start  |
 #define GPIO_PIN_18 GpioAction::BUTTON_PRESS_L3     // L3     | LS     | LS      | L3       | 11     | LS     |
-#define GPIO_PIN_19 GpioAction::BUTTON_PRESS_R3     // R3     | RS     | RS      | R3       | 12     | RS     |
+#define GPIO_PIN_19 GpioAction::BUTTON_PRESS_R3     // R3_2   | RS     | RS      | R3       | 12     | RS     |
 #define GPIO_PIN_20 GpioAction::BUTTON_PRESS_A1     // A1     | Guide  | Home    | PS       | 13     | ~      |
-#define GPIO_PIN_21 GpioAction::BUTTON_PRESS_A2     // A2     | ~      | Capture | ~        | 14     | ~      |
+#define GPIO_PIN_21 GpioAction::BUTTON_PRESS_UP     // UP_2   | UP_2   | UP_2    | UP_2     | UP_2   | UP_2   |
+#define GPIO_PIN_22 GpioAction::BUTTON_PRESS_L3     // L3_1   | LS     | LS      | L3       | 11     | LS     |
+#define GPIO_PIN_23 GpioAction::BUTTON_PRESS_L3     // L3_2   | LS     | LS      | L3       | 11     | LS     |
+#define GPIO_PIN_25 GpioAction::BUTTON_PRESS_R3     // R3_1   | RS     | RS      | R3       | 12     | RS     |
 
 // Setting GPIO pins to assigned by add-on
 //
@@ -39,10 +42,11 @@
 #define GPIO_PIN_01 GpioAction::ASSIGNED_TO_ADDON
 #define GPIO_PIN_15 GpioAction::ASSIGNED_TO_ADDON
 #define GPIO_PIN_28 GpioAction::ASSIGNED_TO_ADDON
+#define GPIO_PIN_29 GpioAction::ASSIGNED_TO_ADDON
 
 // Keyboard Mapping Configuration
 //                                            // GP2040 | Xinput | Switch  | PS3/4/5  | Dinput | Arcade |
-#define KEY_BUTTON_S2   HID_KEY_1             // S2     | Start  | Plus    | Start    | 10     | Start  |
+#define KEY_DPAD_UP     HID_KEY_ARROW_UP      // UP_1   | UP_1   | UP_1    | UP_1     | UP_1   | UP_1   |
 #define KEY_DPAD_DOWN   HID_KEY_ARROW_DOWN    // DOWN   | DOWN   | DOWN    | DOWN     | DOWN   | DOWN   |
 #define KEY_DPAD_RIGHT  HID_KEY_ARROW_RIGHT   // RIGHT  | RIGHT  | RIGHT   | RIGHT    | RIGHT  | RIGHT  |
 #define KEY_DPAD_LEFT   HID_KEY_ARROW_LEFT    // LEFT   | LEFT   | LEFT    | LEFT     | LEFT   | LEFT   |
@@ -55,11 +59,14 @@
 #define KEY_BUTTON_R1   HID_KEY_SPACE         // R1     | RB     | R       | R1       | 6      | P3     |
 #define KEY_BUTTON_L1   HID_KEY_C             // L1     | LB     | L       | L1       | 5      | P4     |
 #define KEY_BUTTON_S1   HID_KEY_5             // S1     | Back   | Minus   | Select   | 9      | Coin   |
-#define KEY_DPAD_UP     HID_KEY_ARROW_UP      // UP     | UP     | UP      | UP       | UP     | UP     |
+#define KEY_BUTTON_S2   HID_KEY_1             // S2     | Start  | Plus    | Start    | 10     | Start  |
 #define KEY_BUTTON_L3   HID_KEY_EQUAL         // L3     | LS     | LS      | L3       | 11     | LS     |
-#define KEY_BUTTON_R3   HID_KEY_MINUS         // R3     | RS     | RS      | R3       | 12     | RS     |
+#define KEY_BUTTON_R3   HID_KEY_MINUS         // R3_2   | RS     | RS      | R3       | 12     | RS     |
 #define KEY_BUTTON_A1   HID_KEY_9             // A1     | Guide  | Home    | PS       | 13     | ~      |
-#define KEY_BUTTON_A2   HID_KEY_F2            // A2     | ~      | Capture | ~        | 14     | ~      |
+#define KEY_DPAD_UP     HID_KEY_ARROW_UP      // UP_2   | UP_2   | UP_2    | UP_2     | UP_2   | UP_2   |
+#define KEY_BUTTON_L3   HID_KEY_EQUAL         // L3_1   | LS     | LS      | L3       | 11     | LS     |
+#define KEY_BUTTON_L3   HID_KEY_EQUAL         // L3_2   | LS     | LS      | L3       | 11     | LS     |
+#define KEY_BUTTON_R3   HID_KEY_MINUS         // R3_1   | RS     | RS      | R3       | 12     | RS     |
 #define KEY_BUTTON_FN   -1                    // Hotkey Function                                        |
 
 #define TURBO_ENABLED 1
